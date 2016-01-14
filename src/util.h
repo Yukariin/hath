5f8ed09ee359a14040b7edfd9e536e89762122fa
Yukarin <yukariin@yandex.ru>
2016-01-14 22:56:03 +0300

    Add base SHA1 algo from Nayuki's project

--- src/util.h
+++ src/util.h
@@ -9,6 +9,9 @@
 #endif
 
 #include <string>
+#include <sstream>
+#include <stdint.h>
+#include <iomanip>
 
 // Case-insensitive comparator.
 struct ci_less : std::binary_function<std::string, std::string, bool>
@@ -18,5 +21,15 @@ struct ci_less : std::binary_function<std::string, std::string, bool>
         return strcasecmp(lhs.c_str(), rhs.c_str()) < 0;
     }
 };
+
+
+/* SHA-1 hash
+ *
+ * Copyright (c) 2014 Project Nayuki
+ * http://www.nayuki.io/page/fast-sha1-hash-implementation-in-x86-assembly
+ */
+void sha1_compress(uint32_t state[5], const uint8_t block[64]);
+void get_sha1_hash(const uint8_t *message, uint32_t len, uint32_t hash[5]);
+std::string get_sha1_string(std::string s);
 
 #endif //HATH_UTIL_H
