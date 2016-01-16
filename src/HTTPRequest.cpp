a254b5dded7fc80fadcd9bf60aad80175e13aa01
Yukarin <yukariin@yandex.ru>
2016-01-16 16:14:21 +0300

    header -> headers

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -1,7 +1,7 @@
 #include "HTTPRequest.h"
 
-HTTPRequest::HTTPRequest(std::string method, std::string url, header_map header, std::vector<char> body, bool upgrade)
-    : method(method), url(url), header(header), body(body), upgrade(upgrade)
+HTTPRequest::HTTPRequest(std::string method, std::string url, header_map headers, std::vector<char> body, bool upgrade)
+    : method(method), url(url), headers(headers), body(body), upgrade(upgrade)
 {
 
 }
--- src/HTTPRequest.h
+++ src/HTTPRequest.h
@@ -12,12 +12,12 @@ class HTTPRequest
 public:
     typedef std::map<std::string, std::string, ci_less> header_map;
 
-    HTTPRequest(std::string method = "", std::string url = "", header_map header = header_map{}, std::vector<char> body = {}, bool upgrade = false);
+    HTTPRequest(std::string method = "", std::string url = "", header_map headers = header_map{}, std::vector<char> body = {}, bool upgrade = false);
     virtual ~HTTPRequest();
 
     std::string method;
     std::string url;
-    std::map<std::string, std::string, ci_less> header;
+    std::map<std::string, std::string, ci_less> headers;
     std::vector<char> body;
 
     bool upgrade;
