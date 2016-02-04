3785dd21da91b6a4f78d38b40a9c154f27b0a634
Yukarin <yukariin@yandex.ru>
2016-02-04 14:04:54 +0300

    Adjust to use in HTTP client

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -1,12 +1,39 @@
 #include "HTTPRequest.h"
 
-HTTPRequest::HTTPRequest(std::string method, std::string url, header_map headers, std::vector<char> body, bool upgrade)
-    : method(method), url(url), headers(headers), body(body), upgrade(upgrade)
+HTTPRequest::HTTPRequest(std::string method, std::string url, std::string host, header_map headers, std::vector<char> body, bool upgrade)
+    : method(method), url(url), host(host), headers(headers), body(body), upgrade(upgrade)
 {
 
 }
 
-HTTPRequest::~HTTPRequest()
+std::shared_ptr<HTTPRequest> HTTPRequest::header(std::string field, std::string value)
 {
+    headers.insert(std::pair<std::string, std::string>(field, value));
 
+    return shared_from_this();
+}
+
+std::vector<char> HTTPRequest::toHTTP()
+{
+    std::stringstream ss;
+
+    // Method and path
+    ss << method << " " << url << " HTTP/1.1\r\n";
+
+    // Generate the Date header on the fly
+    ss << "Host: " << host << "\r\n";
+
+    // Headers
+    for(auto it : headers)
+        ss << it.first << ": " << it.second << "\r\n";
+
+    // A blank line terminates the header section
+    ss << "\r\n";
+
+    std::string headers_str = ss.str();
+    std::vector<char> data(headers_str.begin(), headers_str.end());
+
+    data.insert(data.end(), body.begin(), body.end());
+
+    return data;
 }
--- src/HTTPRequest.h
+++ src/HTTPRequest.h
@@ -3,20 +3,25 @@
 
 #include <string>
 #include <map>
+#include <memory>
 #include <vector>
 
 #include "util.h"
 
-class HTTPRequest
+class HTTPRequest : public std::enable_shared_from_this<HTTPRequest>
 {
 public:
     typedef std::map<std::string, std::string, ci_less> header_map;
 
-    HTTPRequest(std::string method = "", std::string url = "", header_map headers = header_map{}, std::vector<char> body = {}, bool upgrade = false);
-    virtual ~HTTPRequest();
+    HTTPRequest(std::string method = "", std::string url = "", std::string host = "", header_map headers = header_map{}, std::vector<char> body = {}, bool upgrade = false);
+
+    std::shared_ptr<HTTPRequest> header(std::string field, std::string value);
+
+    std::vector<char> toHTTP();
 
     std::string method;
     std::string url;
+    std::string host;
     std::map<std::string, std::string, ci_less> headers;
     std::vector<char> body;
 
