6f1013e505afc32e8f0b4731d04414eee9424662
Yukarin <yukariin@yandex.ru>
2016-02-05 12:50:55 +0300

    Remove cout

--- src/HTTPConnectionManager.cpp
+++ src/HTTPConnectionManager.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
 #include "HTTPConnectionManager.h"
 
 void HTTPConnectionManager::start(connectionPtr c)
 {
     connections.insert(c);
     c->start();
-    std::cout << "Connections: " << connections.size() << std::endl;
 }
 
 void HTTPConnectionManager::stop(connectionPtr c)
