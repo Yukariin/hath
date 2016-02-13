295fbd273c5ad0b5a3e51c6ad3502b48d52b0710
Yukarin <yukariin@yandex.ru>
2016-02-14 00:21:46 +0300

    Use new parser

--- src/HTTPClient.cpp
+++ src/HTTPClient.cpp
@@ -21,18 +21,16 @@ std::shared_ptr<HTTPResponse> HTTPClient::get(std::string url)
 
     write(socket, buffer(r->toHTTP()));
 
-    HTTPParser p;
+    HTTPParser p(HTTPParser::Response);
     std::array<char, 8192> buf;
     asio::error_code err;
     do
     {
-        read(socket, buffer(buf), err);
-        p.parseResponse(buf.data(), buf.size());
+        size_t readed = read(socket, buffer(buf), err);
+        p.parseChunk(buf.data(), readed);
     } while (err != asio::error::eof);
     
-    auto res = p.getResponse();
-    
-    Out::info(r->url.pathAndQuery() + ": Retrieving " + std::to_string(res->body.size()) + " bytes...");
+    Out::info(r->url.pathAndQuery() + ": Retrieving " + std::to_string(p.res->body.size()) + " bytes...");
 
-    return res;
+    return p.res;
 }
